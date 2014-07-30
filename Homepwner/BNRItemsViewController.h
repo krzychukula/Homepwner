c6f91ee658b6807fcd86166c35e81538bcaef84f
Krzysztof Kula <krzychukula@gmail.com>
2014-07-30 08:14:10 +0200

    page 163, empty table view

---
+++ Homepwner/BNRItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRItemsViewController.h
+//  Homepwner
+//
+//  Created by Krzysztof Kula on 30.07.2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRItemsViewController : UITableViewController
+
+@end
---
+++ Homepwner/BNRItemsViewController.m
@@ -0,0 +1,24 @@
+//
+//  BNRItemsViewController.m
+//  Homepwner
+//
+//  Created by Krzysztof Kula on 30.07.2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRItemsViewController.h"
+
+@implementation BNRItemsViewController
+
+- (instancetype)init
+{
+    self = [super initWithStyle:UITableViewStylePlain];
+    return self;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+@end
