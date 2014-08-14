7c2a0e136040f10b1ad0900a9e91bf579a24b04a
Krzysztof Kula <krzychukula@gmail.com>
2014-08-14 10:34:36 +0200

    page 190 - end od chapter 9. bronze challenge

--- Homepwner/BNRItemsViewController.m
+++ Homepwner/BNRItemsViewController.m
@@ -124,4 +124,9 @@ moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath
 {
     [[BNRItemStore sharedStore] moveItemAtIndex:sourceIndexPath.row toIndex:destinationIndexPath.row];
 }
+
+- (NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return @"Remove";
+}
 @end
