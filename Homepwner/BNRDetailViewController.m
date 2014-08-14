e76dd21ca498345b49d5fd7f3b7b59b8bb91f076
Krzysztof Kula <krzychukula@gmail.com>
2014-08-14 13:47:56 +0200

    page 206 - titles

--- Homepwner/BNRDetailViewController.m
+++ Homepwner/BNRDetailViewController.m
@@ -19,6 +19,12 @@
 
 @implementation BNRDetailViewController
 
+- (void)setItem:(BNRItem *)item
+{
+    _item = item;
+    self.navigationItem.title = _item.itemName;
+}
+
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
