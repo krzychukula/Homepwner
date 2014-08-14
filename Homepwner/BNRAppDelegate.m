58adca31764f575d78dcdb8758515e9a2e9616da
Krzysztof Kula <krzychukula@gmail.com>
2014-08-14 12:38:44 +0200

    page 196. chapter 10. UINavigationController

--- Homepwner/BNRAppDelegate.m
+++ Homepwner/BNRAppDelegate.m
@@ -19,7 +19,9 @@
     BNRItemsViewController *itemsViewController;
     itemsViewController = [[BNRItemsViewController alloc] init];
     
-    self.window.rootViewController = itemsViewController;
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:itemsViewController];
+    
+    self.window.rootViewController = navController;
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
