f47f60af39db6e3b57d6c9c6059e74810413e212
krzychukula <krzychukula@gmail.com>
2014-10-19 17:51:19 +0200

    chapter 19. custom UITableViewCell

---
+++ Homepwner/BNRItemCell.h
@@ -0,0 +1,13 @@
+//
+//  BNRItemCell.h
+//  Homepwner
+//
+//  Created by Krzysztof Kula on 19/10/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItemCell : UITableViewCell
+
+@end
---
+++ Homepwner/BNRItemCell.m
@@ -0,0 +1,13 @@
+//
+//  BNRItemCell.m
+//  Homepwner
+//
+//  Created by Krzysztof Kula on 19/10/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRItemCell.h"
+
+@implementation BNRItemCell
+
+@end
