aeccf9f561fbe0b5f98f05dc0864f0133ecfddc5
krzychukula <krzychukula@gmail.com>
2014-10-28 07:50:53 +0100

    page 399, scale the image

--- Homepwner/BNRItemCell.m
+++ Homepwner/BNRItemCell.m
@@ -8,6 +8,13 @@
 
 #import "BNRItemCell.h"
 
+@interface BNRItemCell ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageViewHeightConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageViewWidthConstraint;
+
+@end
+
+
 @implementation BNRItemCell
 
 - (IBAction)showImage:(id)sender
@@ -23,6 +30,26 @@
     self.nameLabel.font = font;
     self.serialNumberLabel.font = font;
     self.valueLabel.font = font;
+    
+    static NSDictionary *imageSizeDictionary;
+    
+    if (!imageSizeDictionary) {
+        imageSizeDictionary = @{
+                                UIContentSizeCategoryExtraSmall: @40,
+                                UIContentSizeCategorySmall: @40,
+                                UIContentSizeCategoryMedium: @40,
+                                UIContentSizeCategoryLarge: @40,
+                                UIContentSizeCategoryExtraLarge: @45,
+                                UIContentSizeCategoryExtraExtraLarge: @55,
+                                UIContentSizeCategoryExtraExtraExtraLarge: @65
+                                };
+    }
+    
+    NSString *userSize = [[UIApplication sharedApplication] preferredContentSizeCategory];
+    
+    NSNumber *imageSize = imageSizeDictionary[userSize];
+    self.imageViewHeightConstraint.constant = imageSize.floatValue;
+    self.imageViewWidthConstraint.constant = imageSize.floatValue;
 }
 
 - (void)awakeFromNib
