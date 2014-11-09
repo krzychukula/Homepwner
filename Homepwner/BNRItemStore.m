7f39e4aeb1c1feba40d2d9346d84d83002dd3bfa
krzychukula <krzychukula@gmail.com>
2014-11-09 14:31:57 +0100

    get asset type & init on first run

--- Homepwner/BNRItemStore.m
+++ Homepwner/BNRItemStore.m
@@ -195,4 +195,45 @@
     }
     return successful;
 }
+
+- (NSArray)allAssetTypes
+{
+    if (!_allAssetTypes) {
+        NSFetchRequest *request = [[NSFetchRequest alloc] init];
+        
+        NSEntityDescription *e = [NSEntityDescription entityForName:@"BNRAssetType"
+                                             inManagedObjectContext:self.context];
+        request.entity = e;
+        
+        NSError *err = nil;
+        NSArray *result = [self.context executeFetchRequest:request
+                                                      error:&err];
+        if (!result) {
+            [NSException raise:@"FetchFailed"
+                        format:@"Reason: %@", [err localizedDescription]];
+        }
+        _allAssetTypes = [result mutableCopy];
+    }
+    
+    //is this the first time the program is being run?
+    if([_allAssetTypes count] == 0){
+        NSManagedObject *type;
+        
+        type = [NSEntityDescription insertNewObjectForEntityForName:@"BNRAssetType"
+                                             inManagedObjectContext:self.context];
+        [type setValue:@"Furniture" forKey:@"label"];
+        [_allAssetTypes addObject:type];
+        
+        type = [NSEntityDescription insertNewObjectForEntityForName:@"BNRAssetType"
+                                             inManagedObjectContext:self.context];
+        [type setValue:@"Jewelry" forKey:@"label"];
+        [_allAssetTypes addObject:type];
+        
+        type = [NSEntityDescription insertNewObjectForEntityForName:@"BNRAssetType"
+                                             inManagedObjectContext:self.context];
+        [type setValue:@"Electronics" forKey:@"label"];
+        [_allAssetTypes addObject:type];
+    }
+}
+
 @end
